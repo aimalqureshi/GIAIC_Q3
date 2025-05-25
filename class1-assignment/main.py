4d90feff6ce5fbe1bfc4b1d2feb8e15a4786b36c
aimalqureshi <aimalqureshi5183@gmail.com>
2025-05-26 03:08:46 +0500

    Add files via upload

---
+++ class1-assignment/main.py
@@ -0,0 +1,31 @@
+# first class
+
+print("Hello World")
+
+
+# Pycache Example
+
+def greet (name):
+    return f"Hello, {name}!"
+print(greet("Aimal Qureshi"))
+   
+
+# data types
+
+name = "Aimal Qureshi"                                          # string
+
+age = 20                                                   # integer
+
+height = 5.5                                                 # float
+
+is_student = True                                           # boolean
+
+grades = [95, 83, 90, 87, 74]                                # list
+
+coordinates = (3, 5)                                         # tuple
+
+person = {"name": "Aimal Qureshi", "age": 20, "height": 5.5}      # dictionary
+                                            
+unique_numbers = {1, 2, 3, 4, 5}                               # set
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
